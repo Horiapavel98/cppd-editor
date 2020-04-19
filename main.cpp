1a914754ce3483288e60a4ebef471360698f3427
Horia Pavel <horiapavel@yahoo.com>
2020-04-19 11:56:20 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+
+    if (argc > 1) {
+        return 1;
+    }
+
+    std::cout << "I'm gonna make something that matters" << std::endl;
+
+    return 0;
+}
